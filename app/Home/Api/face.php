4908782be9d7b4afe2ca7605758ec60dfdf228a9
Zhang Yu Sheng <zzyyss@outlook.com>
2017-04-09 13:13:01 +0800

    Mod: face interface

--- app/Home/Api/face.php
+++ app/Home/Api/face.php
@@ -1,21 +1,30 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 //require '../../../config/config.php';
 require '../../../lib/functions.php';
+
+$img = substr($_POST['image_base64'], 22);
 
 $c_url_data = [ 
     'api_key' => 'v7j0pw-2tWVhRv6_GA0zQGd-SgzFRe3W',
     'api_secret' => '5UsgXX43G6qd2Zz0L1OY-8e1LSeUgSws',
     //'image_url' => 'http://www.thedragonchina.com/Resume/zhangyusheng.jpg'
-    'image_base64' => $_GET['image_base64']
+    'image_base64' => $img
 ];
 
 $c_url = 'https://api-cn.faceplusplus.com/facepp/v3/detect'; 
 
 $result = curl($c_url, $c_url_data);
+//print_r($result);
+//die;
 
+try {
 $face_token = json_decode($result)
     ->faces[0]
     ->face_token;
+} catch (Exception $e) {
+    die('error');
+}
 
 $analyze_url = 'https://api-cn.faceplusplus.com/facepp/v3/face/analyze';
 
@@ -28,4 +37,4 @@ $c_url_data = [
 
 $result = curl($analyze_url, $c_url_data);
 
-echo json_encode($result, true);
+print_r($result);
